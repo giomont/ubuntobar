6b11b49d391f5460dd2356e82066b2f356f8615b
Giovanni Montoya <giomont@gmail.com>
2025-02-03 19:51:02 +0000

    Deploy React app to GitHub Pages

---
+++ src/App.js
@@ -0,0 +1,25 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Ubuntu App<code>src/App.js</code> para hacer los pedidos , actualizar la lista de productos y hacer seguimiento de  las mesas
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
